4ebab1cd632c2e257324f490c199f4fd5a414e85
Ankita Dhiman <ankitadhiman2063@gmail.com>
2023-08-01 17:09:30 +0530

    third

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
-public class Sum {
+public class sum {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 20;
         int b = 10;
         System.out.println(a+b);
     }
